a4083b0871999f30d023d6eb27eca391b8fa0318
Nha Nguyen <nguyenvannha1910@gmail.com>
2023-02-08 16:28:16 +0700

    update-1

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from boards import views
 
 urlpatterns = [
+    path(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
