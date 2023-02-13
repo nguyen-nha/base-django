d40661bb1079de374cf6976515e4fe732d51457a
Nha Nguyen <nguyenvannha1910@gmail.com>
2023-02-13 17:23:46 +0700

    update 23/02

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import Http404
 from boards.models import Board
 
 
@@ -8,3 +8,10 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 # Create your views here.
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
--- myproject/urls.py
+++ myproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    path('home/', views.home, name='home'),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]
